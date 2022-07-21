73a1f6463ba39f435be06f7bd99c453ea0402e45
081dikeh <98462814+081dikeh@users.noreply.github.com>
2022-07-21 22:25:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let menu = document.querySelector(".menu")
+let open = document.querySelector("#open")
+
+menu.onclick function(){
+    
+}
+
+
+function appear() {
+    menu.style.right = "0px";
+    menu.style.display = "block"
+}
+
+function appear() {
+    var x = document.querySelector(".menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
